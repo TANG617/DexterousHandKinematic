ad447183b38621b8a0d08e050efb0d42965de1b8
LiTang <litang0617@outlook.com>
2024-11-27 14:25:01 +0800

    rebuild with class

--- main.py
+++ main.py
@@ -17,48 +17,62 @@ h = 30
 # alpha = float(math.pi/3)
 alpha = 0
 
-def create_rotation_matrix(q1, q2):
-    R = torch.tensor([
-        [math.cos(q2),      math.sin(q2)*math.sin(q1),    math.sin(q2)*math.cos(q1)],
-        [0,                 math.cos(q1),                  -math.sin(q1)],
-        [-math.sin(q2),     math.cos(q2)*math.sin(q1),    math.cos(q2)*math.cos(q1)]
-    ], dtype=torch.float32)
-    
-    return R
+class DexterousHandKinematics:
+    def __init__(self, ax, ay, bx, by, p, q1, q2, l1, l2, l3, l4, cy, h, alpha):
+        self.ax = ax
+        self.ay = ay
+        self.bx = bx
+        self.by = by
+        self.p = p
+        self.q1 = q1
+        self.q2 = q2
+        self.l1 = l1
+        self.l2 = l2
+        self.l3 = l3
+        self.l4 = l4
+        self.cy = cy
+        self.h = h
+        self.alpha = alpha
 
-def create_argument(ax,ay,bx,by,p,R):
-
-    a = torch.tensor([[ax,ay,0],[-ax,ay,0]],dtype=torch.float32)
-    b_ = torch.tensor([[bx,by,0],[-bx,by,0]],dtype=torch.float32)
-    p = torch.tensor([[0,0,p]],dtype=torch.float32)
-    k = torch.cat([p + R @ b_[0] - a[0], p + R @ b_[1] - a[1]], dim=0)
-    return k
+        self.create_rotation_matrix()
+        self.create_argument()
+        self.calculate_d12()
+        self.calculate_d3()
 
 
-def calculate_d12(d, k, l1):
-    k_norm_squared = torch.sqrt(torch.sum(k**2, dim=1))
-    d[0][2] = k[0][2] - torch.sqrt(l1**2 + k[0][2]**2 - k_norm_squared[0]**2)
-    d[1][2] = k[1][2] - torch.sqrt(l1**2 + k[1][2]**2 - k_norm_squared[1]**2)
-    return d
+    def create_rotation_matrix(self):
+        self.R = torch.tensor([
+            [math.cos(self.q2),      math.sin(self.q2)*math.sin(self.q1),    math.sin(self.q2)*math.cos(self.q1)],
+            [0,                 math.cos(self.q1),                  -math.sin(self.q1)],
+            [-math.sin(self.q2),     math.cos(self.q2)*math.sin(self.q1),    math.cos(self.q2)*math.cos(self.q1)]
+        ], dtype=torch.float32)
 
-def calculate_d3(d, h,q1,q2,alpha,l3,l4, p,cy):
-    P12_x = torch.tensor(h*math.sin(q2)*math.sin(q1) - l4*math.sin(q2)*math.sin(q1-alpha))
-    P12_y = torch.tensor(h*math.cos(q1) - l4*math.cos(q1-alpha) - cy)
-    P12_z = torch.tensor(h*math.cos(q2)*math.sin(q1) - l4*math.cos(q2)*math.sin(q1 - alpha) + p)
-    A1 = P12_z
-    B1 = P12_x**2 + P12_y**2 + P12_z**2 - l3**2
-    d[2][2] = A1 - torch.sqrt(A1**2 - B1)
-    return d
+    def create_argument(self):
+
+        a = torch.tensor([[self.ax,self.ay,0],[-self.ax,self.ay,0]],dtype=torch.float32)
+        b_ = torch.tensor([[self.bx,self.by,0],[-self.bx,self.by,0]],dtype=torch.float32)
+        p = torch.tensor([[0,0,self.p]],dtype=torch.float32)
+        self.k = torch.cat([p + self.R @ b_[0] - a[0], p + self.R @ b_[1] - a[1]], dim=0)
+
+
+    def calculate_d12(self):
+        self.d = torch.zeros(3,3,dtype=torch.float32)
+        k_norm_squared = torch.sqrt(torch.sum(self.k**2, dim=1))
+        self.d[0][2] = self.k[0][2] - torch.sqrt(self.l1**2 + self.k[0][2]**2 - k_norm_squared[0]**2)
+        self.d[1][2] = self.k[1][2] - torch.sqrt(self.l1**2 + self.k[1][2]**2 - k_norm_squared[1]**2)
+
+    def calculate_d3(self):
+        P12_x = torch.tensor(self.h*math.sin(self.q2)*math.sin(self.q1) - self.l4*math.sin(self.q2)*math.sin(self.q1-self.alpha))
+        P12_y = torch.tensor(self.h*math.cos(self.q1) - self.l4*math.cos(self.q1-self.alpha) - self.cy)
+        P12_z = torch.tensor(self.h*math.cos(self.q2)*math.sin(self.q1) - self.l4*math.cos(self.q2)*math.sin(self.q1 - self.alpha) + self.p)
+        A1 = P12_z
+        B1 = P12_x**2 + P12_y**2 + P12_z**2 - self.l3**2
+        self.d[2][2] = A1 - torch.sqrt(A1**2 - B1)
 
 def main():
-    R = create_rotation_matrix(q1=q1, q2=q2)
-    k = create_argument(ax=ax,ay=ay,bx=bx,by=by,p=p, R=R)
-    d = torch.zeros(3,3,dtype=torch.float32)
-    d = calculate_d12(d, k, l1)
-    d = calculate_d3(d, h,q1,q2,alpha,l3,l4, p,cy)
-    print("d1:",d[0][2].item())
-    print("d2:",d[1][2].item())
-    print("d3:",d[2][2].item())
+    dhk = DexterousHandKinematics(ax,ay,bx,by,p,q1,q2,l1,l2,l3,l4,cy,h,alpha)
+    print(dhk.d)
+    
 if __name__ == "__main__":
     main()
 
